17f1ba02100848309b29a8b12fc2405b323c50e5
Elliott <elliottrecord@gmail.com>
2018-12-05 20:33:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHeartRegular, faHeartSolid);
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {};
+
+	render() {
+		return (
+			<React.Fragment>
+				<main className='container'>
+					<h1>Hello world</h1>
+				</main>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
