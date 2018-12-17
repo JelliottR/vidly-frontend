3c12ba94d3d923d496cd025762457cf5d24d6a39
Elliott <elliottrecord@gmail.com>
2018-12-17 18:38:17 +0000

    Implemeted ListGroup and made it reuseable.

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Like from './common/like';
 import Pagination from './common/pagination';
+import ListGroup from './common/listGroup';
 import { paginate } from '../utils/paginate';
 import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
 
 class Movies extends Component {
-	state = { movies: getMovies(), pageSize: 4, currentPage: 1 };
+	state = { movies: [], genres: [], pageSize: 4, currentPage: 1 };
+
+	componentDidMount() {
+		this.setState({ movies: getMovies(), genres: getGenres() });
+	}
 
 	handleDelete(movie) {
 		const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -23,6 +29,10 @@ class Movies extends Component {
 		this.setState({ currentPage: page });
 	};
 
+	handleGenreSelect = (genre) => {
+		console.log(genre);
+	};
+
 	render() {
 		const { length: count } = this.state.movies;
 		const { pageSize, currentPage, movies: allMovies } = this.state;
@@ -31,46 +41,59 @@ class Movies extends Component {
 		const movies = paginate(allMovies, currentPage, pageSize);
 
 		return (
-			<React.Fragment>
-				<p>Showing {count} movies in the database.</p>
+			<div className='row'>
+				<div className='col-3'>
+					<ListGroup
+						items={this.state.genres}
+						textProperty='name'
+						valueProperty='_id'
+						onItemSelect={this.handleGenreSelect}
+					/>
+				</div>
+				<div className='col'>
+					<p>Showing {count} movies in the database.</p>
 
-				<table className='table'>
-					<thead>
-						<tr>
-							<th scope='col'>Title</th>
-							<th scope='col'>Genre</th>
-							<th scope='col'>Stock</th>
-							<th scope='col'>Rate</th>
-							<th scope='col'>Rate</th>
-							<th scope='col' />
-						</tr>
-					</thead>
-					<tbody>
-						{movies.map((movie) => (
-							<tr key={movie._id}>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td>
-									<Like liked={movie.liked} onLiked={() => this.handleLiked(movie)} />
-								</td>
-								<td>
-									<button className='btn btn-sm btn-danger' onClick={() => this.handleDelete(movie)}>
-										Delete
-									</button>
-								</td>
+					<table className='table'>
+						<thead>
+							<tr>
+								<th scope='col'>Title</th>
+								<th scope='col'>Genre</th>
+								<th scope='col'>Stock</th>
+								<th scope='col'>Rate</th>
+								<th scope='col'>Rate</th>
+								<th scope='col' />
 							</tr>
-						))}
-					</tbody>
-				</table>
-				<Pagination
-					itemsCount={count}
-					pageSize={pageSize}
-					currentPage={currentPage}
-					onPageChange={this.handlePageChange}
-				/>
-			</React.Fragment>
+						</thead>
+						<tbody>
+							{movies.map((movie) => (
+								<tr key={movie._id}>
+									<td>{movie.title}</td>
+									<td>{movie.genre.name}</td>
+									<td>{movie.numberInStock}</td>
+									<td>{movie.dailyRentalRate}</td>
+									<td>
+										<Like liked={movie.liked} onLiked={() => this.handleLiked(movie)} />
+									</td>
+									<td>
+										<button
+											className='btn btn-sm btn-danger'
+											onClick={() => this.handleDelete(movie)}
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					<Pagination
+						itemsCount={count}
+						pageSize={pageSize}
+						currentPage={currentPage}
+						onPageChange={this.handlePageChange}
+					/>
+				</div>
+			</div>
 		);
 	}
 }
