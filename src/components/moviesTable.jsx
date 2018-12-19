05133f4404384ce7917be5bd778207e0a0f4e088
Elliott <elliottrecord@gmail.com>
2018-12-19 14:33:10 +0000

    Added content to Like and Delete, and rendered them conditionly.

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -9,8 +9,15 @@ class MoviesTable extends Component {
 		{ path: 'genre.name', label: 'Genre' },
 		{ path: 'numberInStock', label: 'Stock' },
 		{ path: 'dailyRentalRate', label: 'Rate' },
-		{ key: 'like' },
-		{ key: 'delete' },
+		{ key: 'like', content: (movie) => <Like liked={movie.liked} onLiked={() => this.props.onLiked(movie)} /> },
+		{
+			key: 'delete',
+			content: (movie) => (
+				<button className='btn btn-sm btn-danger' onClick={() => this.props.onDelete(movie)}>
+					Delete
+				</button>
+			),
+		},
 	];
 
 	render() {
@@ -19,25 +26,7 @@ class MoviesTable extends Component {
 		return (
 			<table className='table'>
 				<TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort} />
-				<TableBody data={movies} />
-				<tbody>
-					{movies.map((movie) => (
-						<tr key={movie._id}>
-							<td>{movie.title}</td>
-							<td>{movie.genre.name}</td>
-							<td>{movie.numberInStock}</td>
-							<td>{movie.dailyRentalRate}</td>
-							<td>
-								<Like liked={movie.liked} onLiked={() => onLiked(movie)} />
-							</td>
-							<td>
-								<button className='btn btn-sm btn-danger' onClick={() => onDelete(movie)}>
-									Delete
-								</button>
-							</td>
-						</tr>
-					))}
-				</tbody>
+				<TableBody columns={this.columns} data={movies} />
 			</table>
 		);
 	}
